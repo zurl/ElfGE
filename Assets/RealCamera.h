90f289cdad023e901427f65c7ea025d41c2a9093
lulufa390 <1042767545@qq.com>
2017-12-26 14:28:34 +0800

    /

---
+++ Assets/RealCamera.h
@@ -0,0 +1,23 @@
+//
+// Created by lulufa on 2017/12/26.
+//
+
+#ifndef ELFGE_REALCAMERA_H
+#define ELFGE_REALCAMERA_H
+
+#include "StandardAssets.h"
+
+class RealCamera : public FirstPlayerCamera{
+public:
+    GameObject * player;
+
+    void update() override{
+
+    }
+
+
+};
+
+
+
+#endif //ELFGE_REALCAMERA_H
