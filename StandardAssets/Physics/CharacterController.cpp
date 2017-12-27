d178b3bf7fc8f1bf6bbb59407ac87aeaf79970ab
zcy <zurl@live.com>
2017-12-27 15:32:19 +0800

    fucking stable

---
+++ StandardAssets/Physics/CharacterController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 张程易 on 27/12/2017.
+//
+
+#include "CharacterController.h"
---
+++ StandardAssets/Physics/CharacterController.h
@@ -0,0 +1,14 @@
+//
+// Created by 张程易 on 27/12/2017.
+//
+
+#ifndef ELFGE_CHARACTERCONTROLLER_H
+#define ELFGE_CHARACTERCONTROLLER_H
+
+
+class CharacterController {
+
+};
+
+
+#endif //ELFGE_CHARACTERCONTROLLER_H
