4dfab41ef9c7cc51760f9856dd0534daa003546d
zcy <zurl@live.com>
2018-01-13 10:22:52 +0800

    Almost done

---
+++ Assets/Scripts/BoomScript.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 张程易 on 12/01/2018.
+//
+
+#include "BoomScript.h"
---
+++ Assets/Scripts/BoomScript.h
@@ -0,0 +1,14 @@
+//
+// Created by 张程易 on 12/01/2018.
+//
+
+#ifndef ELFGE_BOOMSCRIPT_H
+#define ELFGE_BOOMSCRIPT_H
+
+
+class BoomScript {
+
+};
+
+
+#endif //ELFGE_BOOMSCRIPT_H
